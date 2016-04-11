dfba3c8e5bb1ec0d7b9cd05b95eb9f7e8dad7e21
weakiwi <670932228@qq.com>
2016-04-11 21:52:19 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,11 +6,11 @@ echo -e "2) install vim and spf13 \n\n"
 echo -e "3) install zsh and oh-my-zsh \n\n"
 echo -e "4) install all of this \n\n"
 read -p "Please input: " user_input
-if[$user_input="1"]; then
+if[$user_input=="1"]; then
     /bin/bash install_lnmp.sh
-elif[$user_input="2"]; then
+elif[$user_input=="2"]; then
     /bin/bash install_vim.sh
-elif[$user_input="3"]; then
+elif[$user_input=="3"]; then
     /bin/bash install_zsh.sh
 else
     echo -e "input error"

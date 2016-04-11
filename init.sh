94e34ba2b2c40124e1d0c34fe79fd79aa6e6b95a
weakiwi <670932228@qq.com>
2016-04-11 21:20:45 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,11 +10,11 @@ echo -e "2) install vim and spf13 \n\n"
 echo -e "3) install zsh and oh-my-zsh \n\n"
 echo -e "4) install all of this \n\n"
 read -p "Please input: " user_input
-if[$user_input = 1];then
+if[$user_input=1]; then
     wget -c https://api.sinas3.com/v1/SAE_lnmp/soft/lnmp1.2-full.tar.gz&& tar zxf lnmp1.2-full.tar.gz && cd lnmp1.2-full && ./install.sh lnmp
-if[$user_input = 2];then
+if[$user_input=2]; then
     curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
-if[$user_input = 3];then
+if[$user_input=3]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
     echo -e "input error"

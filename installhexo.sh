87dd84efadc4dc3457f8b03ffbbdb7c00cc85bf2
weakiwi <670932228@qq.com>
2016-08-14 14:45:10 +0800

    Update installhexo.sh

--- installhexo.sh
+++ installhexo.sh
@@ -1,9 +1,6 @@
-npm install -g cnpm --registry=https://registry.npm.taobao.org
-ln -s /usr/bin/nodejs /usr/bin/node
-/usr/bin/npm install -g hexo
-mkdir hexo
-chmod 644 -R hexo
+sudo mkdir hexo
+sudo chmod 644 -R hexo
 cd hexo
-/usr/bin/hexo init
-/usr/bin/git clone https://github.com/litten/hexo-theme-yilia.git themes/yilia
-rm _config.yml
+sudo /usr/bin/hexo init
+sudo /usr/bin/git clone https://github.com/litten/hexo-theme-yilia.git themes/yilia
+sudo rm _config.yml

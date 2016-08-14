3f331d41e69557caea3738a815af85bd6cd37c7b
weakiwi <670932228@qq.com>
2016-08-14 14:49:23 +0800

    Update installhexo.sh

--- installhexo.sh
+++ installhexo.sh
@@ -1,6 +1,6 @@
-sudo mkdir hexo
-sudo chmod 644 -R hexo
+mkdir hexo
+chmod 644 -R hexo
 cd hexo
-sudo /usr/bin/hexo init
-sudo /usr/bin/git clone https://github.com/litten/hexo-theme-yilia.git themes/yilia
-sudo rm _config.yml
+/usr/bin/hexo init
+/usr/bin/git clone https://github.com/litten/hexo-theme-yilia.git themes/yilia
+rm _config.yml

a51d84f39a3d7537ae9835060ecd163a1cf172de
weakiwi <670932228@qq.com>
2016-08-14 10:43:37 +0800

    Update installhexo.sh

--- installhexo.sh
+++ installhexo.sh
@@ -1,7 +1,4 @@
-alias cnpm="npm --registry=https://registry.npm.taobao.org \
---cache=$HOME/.npm/.cache/cnpm \
---disturl=https://npm.taobao.org/dist \
---userconfig=$HOME/.cnpmrc"
+npm install -g cnpm --registry=https://registry.npm.taobao.org
 cnpm install -g hexo
 mkdir hexo
 chmod 644 -R hexo
